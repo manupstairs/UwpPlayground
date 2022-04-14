dd0e990fd078d395f67cef37f2a8ae0901fefd24
xingzheng <xingzheng@outlook.com>
2022-04-14 11:23:26 +0800

    Add project files.

---
+++ WwanTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WwanTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wwanHelper = new WwanHelper();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WwanTest/WwanHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.Connectivity;
+
+namespace WwanTest
+{
+    public class WwanHelper
+    {
+        public WwanHelper()
+        {
+            NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
+        }
+
+        private async void NetworkInformation_NetworkStatusChanged(object sender)
+        {
+            //var filter = new ConnectionProfileFilter { IsWlanConnectionProfile = true };
+            var filter = new ConnectionProfileFilter { IsWwanConnectionProfile = true };
+            var wwanProfiles =
+                await NetworkInformation.FindConnectionProfilesAsync(filter);
+            var wlanProfileList = wwanProfiles.ToList();
+            foreach (var item in wlanProfileList)
+            {
+                Console.WriteLine($"WWAN name: {item.ProfileName}");
+                Console.WriteLine($"AccessPointName: {item.WwanConnectionProfileDetails.AccessPointName}");
+                Console.WriteLine($"CurrentDataClass: {item.WwanConnectionProfileDetails.GetCurrentDataClass()}");
+                Console.WriteLine($"HomeProviderId: {item.WwanConnectionProfileDetails.HomeProviderId}");
+                Console.WriteLine($"NetworkRegistrationState: {item.WwanConnectionProfileDetails.GetNetworkRegistrationState()}");
+            }
+        }
+    }
+}
